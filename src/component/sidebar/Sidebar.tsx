931fdfd9d348fe79ef8ed4c2dadefe7c46944f8a
Sozai83 <chikushi3.227@gmail.com>
2023-06-27 07:38:58 +0900

    removed ; from the component to avoid error

--- src/component/sidebar/Sidebar.tsx
+++ src/component/sidebar/Sidebar.tsx
@@ -2,17 +2,13 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-  <div className="sidebar">
-    {/* sidebarLeft */}
-    <div className="sidebarLeft">
-
+    <div className="sidebar">
+      {/* sidebarLeft */}
+      <div className="sidebarLeft"></div>
+      {/* sidebarRight */}
+      <div className="sidebarRight"></div>
     </div>
-    {/* sidebarRight */}
-    <div className="sidebarRight">
-        
-    </div>
-  </div>;
-  )
+  );
 };
 
 export default Sidebar;
