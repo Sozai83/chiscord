abfaa07c21e4ed99bc6219ea3067c6d74f6addc4
Sozai83 <chikushi3.227@gmail.com>
2023-06-29 08:26:43 +0900

    added side bar

--- src/component/sidebar/Sidebar.tsx
+++ src/component/sidebar/Sidebar.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import "./Sidebar.scss";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       {/* sidebarLeft */}
-      <div className="sidebarLeft"></div>
+      <div className="sidebarLeft">
+        <div className="serverIcon">
+          <img src="logo192.png" alt="" />
+        </div>
+        <div className="serverIcon">
+          <img src="logo192.png" alt="" />
+        </div>
+      </div>
       {/* sidebarRight */}
-      <div className="sidebarRight"></div>
+      <div className="sidebarRight">
+        <div className="serverTop">
+          <h3>Discord</h3>
+        </div>
+      </div>
     </div>
   );
 };
