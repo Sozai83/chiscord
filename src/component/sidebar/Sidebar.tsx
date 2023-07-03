a137356a78e5415b1cee18c99f400d651ce170f8
Sozai83 <chikushi3.227@gmail.com>
2023-07-03 12:17:59 +0900

    added sidebar footer

--- src/component/sidebar/Sidebar.tsx
+++ src/component/sidebar/Sidebar.tsx
@@ -3,6 +3,9 @@ import SidebarChannel from "./SidebarChannel";
 import "./Sidebar.scss";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import AddIcon from "@mui/icons-material/Add";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import SettingsIcon from "@mui/icons-material/Settings";
 
 const Sidebar = () => {
   return (
@@ -36,6 +39,21 @@ const Sidebar = () => {
             <SidebarChannel />
             <SidebarChannel />
             <SidebarChannel />
+          </div>
+
+          <div className="sidebarFooter">
+            <div className="sidebarAccount">
+              <img src="icon.png" alt="" />
+              <div className="accountName">
+                <h4>Chicord</h4>
+                <span>#0227</span>
+              </div>
+            </div>
+            <div className="sidebarVoice">
+              <MicIcon />
+              <HeadphonesIcon />
+              <SettingsIcon />
+            </div>
           </div>
         </div>
       </div>
