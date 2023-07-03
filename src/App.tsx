6630971fbb3344b46024faf6413290cb42b69f57
Sozai83 <chikushi3.227@gmail.com>
2023-07-03 12:34:27 +0900

    added chat component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./App.scss";
 import Sidebar from "./component/sidebar/Sidebar";
+import Chat from "./component/chat/Chat";
 
 function App() {
   return (
     <div className="App">
       {/* side bar */}
       <Sidebar />
+
+      {/* Home Chat */}
+      <div className="chat">
+        <Chat />
+      </div>
     </div>
   );
 }
---
+++ src/component/chat/Chat.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Chat.scss";
+
+type Props = {};
+
+const Chat = (props: Props) => {
+  return (
+    <div className="chat">
+      {/* Chat header */}
+      {/* Chat message */}
+      {/* Chat input */}
+    </div>
+  );
+};
+
+export default Chat;
