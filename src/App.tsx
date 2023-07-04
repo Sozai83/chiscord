ba532eed5a5aff964d3eda0b5bcd42c17af2dda1
Sozai83 <chikushi3.227@gmail.com>
2023-07-04 12:44:11 +0900

    Component - Sidebar - Added onSnapshot to get the channel data

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Chat from "./component/chat/Chat";
 import Login from "./component/login/Login";
 import { auth } from "./firebase";
 import { login, logout } from "./features/userSlice";
+import { ErrorBoundary } from "react-error-boundary";
+import { fallbackRender } from "./utils/ErrorFallback";
 
 function App() {
   const user = useAppSelector((state) => state.user);
@@ -36,8 +38,11 @@ function App() {
     <div className="App">
       {user ? (
         <>
-          {/* side bar */}
-          <Sidebar />
+          <ErrorBoundary FallbackComponent={fallbackRender}>
+            {/* side bar */}
+            <Sidebar />
+          </ErrorBoundary>
+
           {/* Home Chat */}
           <div className="chat">
             <Chat />
