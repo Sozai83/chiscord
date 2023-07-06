d935f1cfbdebb79de22b70743f45f75eaee2d69d
Sozai83 <chikushi3.227@gmail.com>
2023-07-06 11:27:02 +0900

    Chat - Sidebar Right - Created SidebarChannelList component

--- src/component/sidebar/Sidebar.tsx
+++ src/component/sidebar/Sidebar.tsx
@@ -1,29 +1,14 @@
 import React, { useEffect, useState } from "react";
 import SidebarChannel from "./SidebarChannel";
 import "./Sidebar.scss";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import AddIcon from "@mui/icons-material/Add";
-import MicIcon from "@mui/icons-material/Mic";
-import HeadphonesIcon from "@mui/icons-material/Headphones";
-import SettingsIcon from "@mui/icons-material/Settings";
 import { auth, db } from "../../firebase";
 import { useAppSelector } from "../../app/hooks";
 import useCollection from "./../../hooks/useCollection";
 import { addDoc, collection } from "firebase/firestore";
+import SidebarChannelList from "./SidebarChannelList";
 // import { collection, query } from "firebase/firestore/lite";
 
 const Sidebar = () => {
-  const user = useAppSelector((state) => state.user.user);
-  const { documents: channels } = useCollection("channels");
-  const addChannel = async () => {
-    let channelName: string | null = prompt("Add a channel");
-    if (channelName) {
-      await addDoc(collection(db, "channels"), {
-        channelName: channelName,
-      });
-    }
-  };
-
   return (
     <div className="sidebar">
       {/* sidebarLeft */}
@@ -36,49 +21,7 @@ const Sidebar = () => {
         </div>
       </div>
       {/* sidebarRight */}
-      <div className="sidebarRight">
-        <div className="sidebarTop">
-          <h3>Discord</h3>
-          <ExpandMoreIcon />
-        </div>
-        {/* side bar channels */}
-        <div className="sidebarChannels">
-          <div className="sidebarChannelsHeader">
-            <div className="sidebarHeader">
-              <ExpandMoreIcon />
-              <h4>Programming Channel</h4>
-            </div>
-            <AddIcon className="sidebarAddIcon" onClick={addChannel} />
-          </div>
-
-          <div className="sidebarChannelList">
-            {channels.map((channel) => {
-              return (
-                <SidebarChannel
-                  channel={channel}
-                  id={channel.id}
-                  key={channel.id}
-                />
-              );
-            })}
-          </div>
-
-          <div className="sidebarFooter">
-            <div className="sidebarAccount">
-              <img src={user?.photo} alt="" onClick={() => auth.signOut()} />
-              <div className="accountName">
-                <h4>{user?.displayName}</h4>
-                <span>#{user?.uid.substring(0, 4)}</span>
-              </div>
-            </div>
-            <div className="sidebarVoice">
-              <MicIcon />
-              <HeadphonesIcon />
-              <SettingsIcon />
-            </div>
-          </div>
-        </div>
-      </div>
+      <SidebarChannelList />
     </div>
   );
 };
---
+++ src/component/sidebar/SidebarChannelList.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+import SidebarChannel from "./SidebarChannel";
+import "./SidebarChannelList.scss";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import AddIcon from "@mui/icons-material/Add";
+import MicIcon from "@mui/icons-material/Mic";
+import HeadphonesIcon from "@mui/icons-material/Headphones";
+import SettingsIcon from "@mui/icons-material/Settings";
+import { useAppSelector } from "../../app/hooks";
+import useCollection from "../../hooks/useCollection";
+import { addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../firebase";
+
+type Props = {};
+
+const SidebarChannelList = (props: Props) => {
+  const user = useAppSelector((state) => state.user.user);
+  const { documents: channels } = useCollection("channels");
+  const addChannel = async () => {
+    let channelName: string | null = prompt("Add a channel");
+    if (channelName) {
+      await addDoc(collection(db, "channels"), {
+        channelName: channelName,
+      });
+    }
+  };
+
+  return (
+    <div className="sidebarRight">
+      <div className="sidebarTop">
+        <h3>Discord</h3>
+        <ExpandMoreIcon />
+      </div>
+      {/* side bar channels */}
+      <div className="sidebarChannels">
+        <div className="sidebarChannelsHeader">
+          <div className="sidebarHeader">
+            <ExpandMoreIcon />
+            <h4>Programming Channel</h4>
+          </div>
+          <AddIcon className="sidebarAddIcon" onClick={addChannel} />
+        </div>
+
+        <div className="sidebarChannelList">
+          {channels.map((channel) => {
+            return (
+              <SidebarChannel
+                channel={channel}
+                id={channel.id}
+                key={channel.id}
+              />
+            );
+          })}
+        </div>
+
+        <div className="sidebarFooter">
+          <div className="sidebarAccount">
+            <img src={user?.photo} alt="" onClick={() => auth.signOut()} />
+            <div className="accountName">
+              <h4>{user?.displayName}</h4>
+              <span>#{user?.uid.substring(0, 4)}</span>
+            </div>
+          </div>
+          <div className="sidebarVoice">
+            <MicIcon />
+            <HeadphonesIcon />
+            <SettingsIcon />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SidebarChannelList;
